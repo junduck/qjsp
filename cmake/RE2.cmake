ab03abead9472bf508af34df5b7d0384e4a5cf78
Jun <imlijunda@gmail.com>
2026-05-15 14:11:15 +0930

    link to system re2 dynlib

--- cmake/RE2.cmake
+++ cmake/RE2.cmake
@@ -1,9 +1,1 @@
-include(FetchContent)
-
-FetchContent_Declare(
-  re2
-  GIT_REPOSITORY https://github.com/google/re2.git
-  GIT_TAG        2025-11-05
-)
-
-FetchContent_MakeAvailable(re2)
+find_package(re2 REQUIRED)
